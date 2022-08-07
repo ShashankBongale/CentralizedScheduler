15e566a03e49860bc004133641250225b5648510
shashankbongale <shashankbongale@gmail.com>
2022-08-08 00:26:00 +0530

    added code to push task duration to queue in worker

--- Worker.cpp
+++ Worker.cpp
@@ -17,19 +17,70 @@ Worker::~Worker()
 
 void Worker::Run()
 {
-    cout << "Starting worker" << endl;
-    
+    // cout << "Starting worker" << endl;
+    //
+    // zmq::socket_t sock(ctx, zmq::socket_type::rep);
+    // sock.connect("tcp://127.0.0.1:5555");
+    //
+    // zmq::message_t serverMsg;
+    // sock.recv(serverMsg);
+    //
+    // cout << "Received " << serverMsg.to_string() << " from server" << endl;
+    //
+    // string trialMessage = "Message from client";
+    // zmq::message_t clientMsg(trialMessage);
+    //
+    // sock.send(clientMsg, zmq::send_flags::none);
+
+    pthread_t getTaskThread;
+    int iRetVal = pthread_create(&getTaskThread, nullptr, &GetTasks, this);
+    if(!iRetVal)
+    {
+        cout << "Failed to create get task thread" << endl;
+        return;
+    }
+
+    pthread_t performTaskThread;
+    iRetVal = pthread_create(&performTaskThread, nullptr, &PermformTask, this);
+    if(!RetVal)
+    {
+        cout << "Failed to create perform task thread" << endl;
+        return;
+    }
+
+    pthread_join(getTaskThread, nullptr);
+    pthread_join(performTaskThread, nullptr);
+
+}
+
+void* Worker::GetTasks(void* self)
+{
+    Worker* selfObj = (Worker *)self;
+
     zmq::socket_t sock(ctx, zmq::socket_type::rep);
-    sock.connect("tcp://127.0.0.1:5555");
 
-    zmq::message_t serverMsg;
-    sock.recv(serverMsg);
+    string url = "tcp://127.0.0.1:" + selfObj->m_workerSocket;
+    sock.connect(url);
 
-    cout << "Received " << serverMsg.to_string() << " from server" << endl;
+    while(true)
+    {
+        zmq::message_t taskDurationMsg;
+        sock.recv(taskDurationMsg);
 
-    string trialMessage = "Message from cleint";
-    zmq::message_t clientMsg(trialMessage);
+        pthread_mutex_lock(&m_taskQueueLock);
+        string taskDuration = taskDurationMsg.to_string();
+        selfObj->m_taskQueue.push(atoi(taskDuration));
+        pthread_mutex_lock(&m_taskQueueLock);
 
-    sock.send(clientMsg, zmq::send_flags::none);
+        string trialMessage;
+        zmq::message_t clientMsg(trialMessage);
+        sock.send(clientMsg, zmq::send_flags::none);
+
+        if(taskDuration == "0")
+        {
+            cout << "Received termintate msg from master" << endl;
+            break;
+        }
+    }
 
 }
